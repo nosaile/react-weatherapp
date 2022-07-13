a80709b3f38f426947bb5b09d4173c1bd57e6415
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-07-12 19:11:02 -0500

    a weather app using react

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import axios from "axios";
+import {OWM_KEY} from "./keys"
+
+
+function App() {
+
+
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${OWM_KEY}`
+
+  
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        console.log(response.data);
+        setData(response.data)
+      })
+      setLocation('');
+    }
+  }
+  const temperature = Math.floor();
+  const windyness = Math.floor();
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp}</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name != undefined && 
+        <div className="bottom">
+          <div className="feels">
+            {data.main ? <p className='bold'>{data.main.feels_like}</p> : null}
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            {data.wind ? <p className="bold">{data.wind.speed} MPH</p> : null}
+            <p>Wind Speed</p>
+          </div>
+        </div>  
+}
+
+<div className="search">
+        <input type="text" value={location} 
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter Location' />
+      </div>
+
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
