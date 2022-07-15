e1531e79daa09215295598cdf205256801929f67
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-07-15 12:17:49 -0500

    added proper formatting on temperatures and wind speed with what units they are calculated in

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            {data.main ? <h1>{data.main.temp}</h1> : null}
+            {data.main ? <h1>{Math.floor(data.main.temp)}°F</h1> : null}
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
@@ -42,7 +42,7 @@ function App() {
         {data.name != undefined && 
         <div className="bottom">
           <div className="feels">
-            {data.main ? <p className='bold'>{data.main.feels_like}</p> : null}
+            {data.main ? <p className='bold'>{Math.floor(data.main.feels_like)}°F</p> : null}
             <p>Feels Like</p>
           </div>
           <div className="humidity">
@@ -50,7 +50,7 @@ function App() {
             <p>Humidity</p>
           </div>
           <div className="wind">
-            {data.wind ? <p className="bold">{data.wind.speed} MPH</p> : null}
+            {data.wind ? <p className="bold">{Math.floor(data.wind.speed)} MPH</p> : null}
             <p>Wind Speed</p>
           </div>
         </div>  
